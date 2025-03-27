5ff88c8b6abb5bebc72fe897a3cf5939f39af550
yashwanth-kumar-dev <yashwanthkumar6294@gmail.com>
2025-03-27 23:58:39 +0530

    comment added

--- src/components/AgGridWrapper.tsx
+++ src/components/AgGridWrapper.tsx
@@ -1,6 +1,8 @@
 import AgGridAdapter from "./AgGridAdapter";
 import { AgGridAdapterProps } from "../types/gridTypes";
 
+// keeping this wrapper
+// to add extra logic (e.g., additional configuration, state management, logging), makes sense.
 const AgGridWrapper = (props: AgGridAdapterProps) => {
   return <AgGridAdapter {...props} />;
 };
